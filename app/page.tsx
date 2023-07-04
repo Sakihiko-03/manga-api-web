489f9c0a1b9ef9d47e1f63e69eedfd54258b4d41
Supakorn Suriyamongkol <supakornsuriyamongkol@192.168.0.107>
2023-07-03 22:12:41 -0700

    search & categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,16 @@
 'use client'
 import Image from 'next/image'
-// import AniListData from '@/components/anilist_data'
 import { Formik } from 'formik';
 import MangaSearch from '@/components/kitsu';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
-        {/* <div className='text-black'>
-          <h1>My Form</h1>
-          <Formik
-            initialValues={{ name: 'jared' }}
-            onSubmit={(values, actions) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-              }, 1000);
-            }}
-          >
-            {props => (
-              <form onSubmit={props.handleSubmit}>
-                <input
-                  type="text"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.name}
-                  name="name"
-                />
-                {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-                <button type="submit">Submit</button>
-              </form>
-            )}
-          </Formik>
-        </div> */}
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <div className="">
-
           <MangaSearch />
-          {/* <AniListData /> */}
         </div>
       </div>
-
-
-
     </main>
   )
 }
--- components/kitsu.tsx
+++ components/kitsu.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useFormik } from 'formik';
-
+import { useFormik, Formik, Form, Field } from 'formik';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 type resultProps = {
   id: string;
   type: string;
@@ -15,68 +19,82 @@ type Attributes = {
 type Titles = {
   en: string;
   en_jp: string;
-  ja_jp: string;
 };
 
 type PosterImage = {
-  tiny: string;
   large: string;
   small: string;
   medium: string;
-  original: string;
 };
 
 const MangaSearch = () => {
   const [result, setResult] = useState<resultProps[]>([]);
-  useEffect(() => {
-    fetchMangaData(formik.values.searchQuery);
-  }, []);
-
   const formik = useFormik({
     initialValues: {
       searchQuery: '',
+      searchCategories: '',
     },
-    onSubmit: () => {
-    },
+    onSubmit: () => {},
   });
 
-  const fetchMangaData = async (searchQuery: string) => {
+  useEffect(() => {
+    fetchMangaData(formik.values.searchQuery, formik.values.searchCategories);
+  }, [formik.values.searchQuery, formik.values.searchCategories]);
+
+  const fetchMangaData = async (searchQuery: string, searchCategories?: string) => {
     try {
+      let apiUrl = 'https://kitsu.io/api/edge/manga?sort=-averageRating&popularityRank';
+
       if (searchQuery !== '') {
-        const response = await fetch(
-          `https://kitsu.io/api/edge/manga?filter[text]=${searchQuery}`
-        );
-        const mangaData = await response.json();
-        const data = mangaData['data'];
-        setResult(data);
-      } else {
-        const response = await fetch(
-          'https://kitsu.io/api/edge/manga?sort=-averageRating&popularityRank'
-        );
-        const mangaData = await response.json();
-        const data = mangaData['data'];
-        setResult(data);
+        apiUrl = `https://kitsu.io/api/edge/manga?filter[text]=${searchQuery}`;
+
+        if (searchCategories !== '') {
+          apiUrl += `&filter[categories]=${searchCategories}`;
+        }
       }
+
+      const response = await fetch(apiUrl);
+      const mangaData = await response.json();
+      const data = mangaData['data'];
+      setResult(data);
     } catch (error) {
       console.error('Error fetching manga data:', error);
     }
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    formik.handleChange(event);
-    fetchMangaData(value);
+    const { name, value } = event.target;
+    formik.setFieldValue(name, value);
   };
 
   return (
     <div className="text-black flex flex-col">
+      <p>Search Bar</p>
       <input
         id="searchQuery"
         type="text"
         name="searchQuery"
+        placeholder="Title"
         value={formik.values.searchQuery}
         onChange={handleInputChange}
       />
+
+      <FormControl component="fieldset">
+        <FormLabel component="legend">CATEGORIES</FormLabel>
+        <RadioGroup
+          row
+          aria-label="categories"
+          name="searchCategories"
+          value={formik.values.searchCategories}
+          onChange={handleInputChange}
+        >
+          <FormControlLabel value="action" control={<Radio />} label="Action" />
+          <FormControlLabel value="adventure" control={<Radio />} label="Adventure" />
+          <FormControlLabel value="sports" control={<Radio />} label="Sports" />
+          <FormControlLabel value="music" control={<Radio />} label="Music" />
+        </RadioGroup>
+      </FormControl>
+
       <div>
         {result.map((value) => (
           <div className="text-black" key={value.id}>
